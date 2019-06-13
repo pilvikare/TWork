a6b3072d9e7a33f3e00dbf7238e24e2cf821538f
leipomalla <mari.leipola@gmail.com>
2019-06-13 12:37:43 +0300

    Etusivun muotoilut, pathauto paalle

---
+++ themes/custom/TWorktheme/js/main.js
@@ -0,0 +1,12 @@
+var ta = document.getElementById("ta");
+var th = document.getElementById("th");
+
+function thSiirra () {
+    location.href = "twork.test/user/1";
+}
+ function taSiirra () {
+    location.href = "https://www.fazerfoodco.fi/ravintolat/Ravintolat-kaupungeittain/helsinki/opetustalo---paaraide/";
+ }
+
+th.onclick = thSiirra;
+ta.onclick = taSiirra;
